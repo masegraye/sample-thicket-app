ce30bc8e52082b311f5e3751c4267e11800a4ed8
Mason Browne <mason.browne@nebula.com>
2014-08-03 01:26:08 -0700

    Use boostrapper.

--- script/server.js
+++ script/server.js
@@ -1,9 +1,10 @@
-var _       = require("underscore"),
-    Promise = require("bluebird"),
-    thicket   = require("thicket"),
-    Logger  = thicket.c("logger"),
-    CLA     = thicket.c("appenders/console-log"),
-    App     = thicket.c("app");
+var _            = require("underscore"),
+    Promise      = require("bluebird"),
+    thicket      = require("thicket"),
+    Logger       = thicket.c("logger"),
+    CLA          = thicket.c("appenders/console-log"),
+    App          = thicket.c("app"),
+    Bootstrapper = thicket.c("bootstrapper");
 
 var MySampleApp = function() {
   this.initialize.apply(this, arguments);
@@ -12,28 +13,23 @@ var MySampleApp = function() {
 _.extend(MySampleApp.prototype, App.prototype, {
   initialize: function() {
     App.prototype.initialize.apply(this, arguments);
+    this._log = Logger.create("MySampleApp");
   },
-  up: Promise.method(function() {}),
-  down: Promise.method(function() {})
+  up: Promise.method(function() {
+    this._log.debug("Look at me, doing some meaningful setup work...");
+  }),
+  down: Promise.method(function() {
+    this._log.debug("Look at me, doing some meaningful teardown work...");
+  })
 });
 
 Log = Logger.create("Main");
 Logger.root().setLogLevel(Logger.Level.Debug);
 Logger.root().addAppender(new CLA());
 
-var app = new MySampleApp({configuration: {}});
-app.start();
-
-process.on("SIGINT", function() {
-  Promise
-    .attempt(function() {
-      Log.debug("Received SIGINT");
-      return app.stop();
-    })
-    .caught(function(err) {
-      Log.error("Caught error shutting down", err);
-    })
-    .lastly(function() {
-      process.exit(0);
-    });
-});
+var bootstrapper = new Bootstrapper({applicationConstructor: MySampleApp});
+bootstrapper
+  .bootstrap()
+  .then(function(appContainer) {
+    return appContainer.start();
+  });
